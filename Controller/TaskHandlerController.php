9f207b9336c548ef7f72a78d93f332e649804c39
Daniel <daniel.mathis@sulu.io>
2019-10-20 22:30:53 +0200

    refactor controllers

--- Controller/TaskHandlerController.php
+++ Controller/TaskHandlerController.php
@@ -14,19 +14,26 @@ namespace Sulu\Bundle\AutomationBundle\Controller;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use Sulu\Bundle\AutomationBundle\TaskHandler\AutomationTaskHandlerInterface;
 use Sulu\Component\Rest\RequestParametersTrait;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Task\Handler\TaskHandlerFactoryInterface;
 
 /**
  * Provides simple-api for task-handler.
  *
  * @RouteResource("task-handler")
  */
-class TaskHandlerController extends Controller
+class TaskHandlerController
 {
     use RequestParametersTrait;
+
+    protected $taskHandlerFactory;
+
+    public function __construct(TaskHandlerFactoryInterface $taskHandlerFactory)
+    {
+        $this->taskHandlerFactory = $taskHandlerFactory;
+    }
 
     /**
      * @param Request $request
@@ -35,11 +42,10 @@ class TaskHandlerController extends Controller
      */
     public function getAction(Request $request)
     {
-        $handlerFactory = $this->get('task.handler.factory');
         $entityClass = $this->getRequestParameter($request, 'entity-class', true);
 
         $handlers = [];
-        foreach ($handlerFactory->getHandlers() as $handler) {
+        foreach ($this->taskHandlerFactory->getHandlers() as $handler) {
             if ($handler instanceof AutomationTaskHandlerInterface && $handler->supports($entityClass)) {
                 $configuration = $handler->getConfiguration();
                 $handlers[] = ['id' => get_class($handler), 'title' => $configuration->getTitle()];
