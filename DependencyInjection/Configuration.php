71b3d56f589d4d23b9ff63de116643f1039dc5b8
Daniel <daniel.mathis@sulu.io>
2020-06-16 14:29:25 +0200

    fix deprecations

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,8 +26,10 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('sulu_automation')
+        $treeBuilder = new TreeBuilder('sulu_automation');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
             ->children()
                 ->arrayNode('objects')
                     ->addDefaultsIfNotSet()
