9408033599e3144a6a4712882b2d31d8c159ac6b
Luca Rath-Heel <luca@sulu.io>
2021-01-15 10:49:03 +0100

    Replace travis with github actions

--- Tests/Application/bin/console.php
+++ Tests/Application/bin/console.php
@@ -1,22 +1,32 @@
-#!/usr/bin/env php
 <?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArgvInput;
-use Sulu\Bundle\AutomationBundle\Tests\Application\Kernel;
-use Symfony\Component\ErrorHandler\Debug;
-
 set_time_limit(0);
 
-require_once __DIR__ . '/../../bootstrap.php';
+require_once __DIR__ . '/../config/bootstrap.php';
+
+use Sulu\Bundle\AutomationBundle\Tests\Application\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\ErrorHandler\Debug;
 
 $input = new ArgvInput();
-$env = $input->getParameterOption(array('--env', '-e'), getenv('APP_ENV') ?: 'dev');
-$debug = getenv('APP_DEBUG') !== '0' && !$input->hasParameterOption(array('--no-debug', '')) && $env !== 'prod';
+$env = $input->getParameterOption(['--env', '-e'], getenv('SYMFONY_ENV') ?: 'dev');
+$debug = '0' !== getenv('SYMFONY_DEBUG') && !$input->hasParameterOption(['--no-debug', '']) && 'prod' !== $env;
 
 if ($debug) {
     // Clean up when sf 4.3 support is removed
@@ -27,6 +37,6 @@ if ($debug) {
     }
 }
 
-$kernel = new Kernel($env, $debug);
+$kernel = new Kernel($env, $debug, $suluContext);
 $application = new Application($kernel);
 $application->run($input);
